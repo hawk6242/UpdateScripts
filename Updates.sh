e6f52fdb028f5ec1da34ffba2d2ab21b72ee42ad
hawk6242 <clopez624242@gmail.com>
2018-11-04 11:27:48 -0800

    Update Updates.sh

--- Updates.sh
+++ Updates.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt dist-upgrade -y
-sudo apt autoremove -y
+ 
+if [ [ $(lsb_release -rs) > 16 ] ];
+then
+    sudo apt update -y
+    sudo apt upgrade -y
+    sudo apt dist-upgrade -y
+    sudo apt autoremove -y
+else
+    sudo apt-get update -y
+    sudo apt-get upgrade -y
+    sudo apt-get dist-upgrade -y
+    sudo apt-get autoremove -y
+fi
