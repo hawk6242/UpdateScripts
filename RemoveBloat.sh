2509fec587a22a3f6624fde18b62191f74751abe
hawk6242 <clopez624242@gmail.com>
2018-11-05 10:43:27 -0800

    Update RemoveBloat.sh

--- RemoveBloat.sh
+++ RemoveBloat.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-sudo apt purge libreoffice-* transmission-* gnome-mines gnome-mahjongg gnome-orca gnome-sudoku cheese simple-scan aisleriot remmina thunderbird rhythmbox account-plugin* -y
+bloat="libreoffice-* transmission-* gnome-mines gnome-mahjongg gnome-orca gnome-sudoku cheese simple-scan aisleriot remmina thunderbird rhythmbox account-plugin*"
+
+if [[ $(lsb_release -rs) > 16 ]]; then
+	for i in $bloat; do
+		sudo apt purge $i -y
+	done
+	sudo apt autoremove -y
+else
+	for i in $bloat; do
+		sudo apt-get purge $i -y
+	done
+	sudo apt-get autoremove -y
+fi
