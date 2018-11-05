efeb3229094aef44788430b0d261c55f98e0047b
hawk6242 <clopez624242@gmail.com>
2018-11-05 10:18:04 -0800

    Update Updates.sh

--- Updates.sh
+++ Updates.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
  
-# if [ [ $(lsb_release -rs) > 16 ] ];
-# then
-#     sudo apt update -y
-#     sudo apt upgrade -y
-#     sudo apt dist-upgrade -y
-#     sudo apt autoremove -y
-# else
-#     sudo apt-get update -y
-#     sudo apt-get upgrade -y
-#     sudo apt-get dist-upgrade -y
-#     sudo apt-get autoremove -y
-# fi
-
 if [[ $(lsb_release -rs) > 16 ]]; then
-    echo "new"
-else
-    echo "old"
+  sudo apt update -y
+  sudo apt upgrade -y
+  sudo apt dist-upgrade -y
+  sudo apt autoremove -y
+else 
+  sudo apt-get update -y
+  sudo apt-get upgrade -y
+  sudo apt-get dist-upgrade -y
+  sudo apt-get autoremove -y
 fi
