c900712c26ca397ccd60757a8efb82e93646fb61
hawk6242 <clopez624242@gmail.com>
2018-11-10 01:18:29 -0800

    Update Updates.sh

--- Updates.sh
+++ Updates.sh
@@ -2,12 +2,12 @@
  
  #Updates, Upgrades, Distro Upgrade, and autoremoves all irrelevant packages
  
-if [[ $(lsb_release -rs) > 16 ]]; then #This runs if the version of 
+if [[ $(lsb_release -rs) > 16 ]]; then #this will run if the version of Ubuntu is greater than 16 
   sudo apt update -y
   sudo apt upgrade -y
   sudo apt dist-upgrade -y
   sudo apt autoremove -y
-else 
+else #this will run if the version of Ubuntu is less than 16 
   sudo apt-get update -y
   sudo apt-get upgrade -y
   sudo apt-get dist-upgrade -y
