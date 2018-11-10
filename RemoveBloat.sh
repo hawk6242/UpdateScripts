8d8c70e1e9d0702f5127f1c8703ce1a02b07daf1
hawk6242 <clopez624242@gmail.com>
2018-11-10 01:15:48 -0800

    Update RemoveBloat.sh

--- RemoveBloat.sh
+++ RemoveBloat.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+#For Ubuntu Only
+#removes the programs in the following list:
 bloat="libreoffice-* transmission-* gnome-mines gnome-mahjongg gnome-orca gnome-sudoku cheese simple-scan aisleriot remmina thunderbird rhythmbox account-plugin*"
 
-if [[ $(lsb_release -rs) > 16 ]]; then
+
+if [[ $(lsb_release -rs) > 16 ]]; then #this will run if the version of Ubuntu is greater than 16 
 	for i in $bloat; do
 		sudo apt purge $i -y
 	done
 	sudo apt autoremove -y
-else
+else #this will run if the version of Ubuntu is less than 16
 	for i in $bloat; do
 		sudo apt-get purge $i -y
 	done
